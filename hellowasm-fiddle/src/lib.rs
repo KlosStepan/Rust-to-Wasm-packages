d93faddab328a89a5b5b3ed472980b260ed8e63d
Stepan Klos <stepanklos@gmail.com>
2023-10-04 23:36:37 +0200

    Fiddle Hello World changed - TODO adjust

---
+++ hellowasm-fiddle/src/lib.rs
@@ -0,0 +1,16 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn greetStatic() -> String {
+    "Hello from Rust!".to_string()
+}
